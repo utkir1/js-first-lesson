844e6faabd5398e78c30095d4d2e791ed6b08c3e
utkir1 <otkirsoatboyev1@gmail.com>
2022-04-08 23:12:58 +0500

    add something

---
+++ js/script.js
@@ -0,0 +1,8 @@
+alert("Assalomu alaykum, xush kelibsiz");
+var name = prompt("Ismingizni kiriting:");
+var surname = prompt("Familiyangizni kiriting:");
+var age = prompt("Yoshingizni kiriting:");
+var address = prompt("Manzilingizni kiriting:");
+
+alert(`
+ Ism: ${name} \n Familiya: ${surname} \n Yosh: ${age} \n Manzil: ${address}`);
